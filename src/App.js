707b176711540e75857c092badad754fc872bf0e
IrishMan-5403 <irishman5403@gmail.com>
2024-12-31 14:53:44 +0530

    changed file structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
-import Add from './pages/add_form/add_form'
+import Add from "./pages/add_form/add_form";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
-            <Add/>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/new-form" element={<Add />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
